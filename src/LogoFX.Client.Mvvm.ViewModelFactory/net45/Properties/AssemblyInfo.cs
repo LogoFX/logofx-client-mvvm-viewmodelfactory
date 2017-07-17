3cf36d8d5b6bbf73c23a86be887185c75f14c1aa
Gennady Verdel <godrose@gmail.com>
2017-07-17 13:05:03 +0300

    Added net45 and netstandard1.3 projects; removed portable.

--- src/LogoFX.Client.Mvvm.ViewModelFactory/net45/Properties/AssemblyInfo.cs
+++ src/LogoFX.Client.Mvvm.ViewModelFactory/net45/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
-﻿using System.Resources;
-using System.Reflection;
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("LogoFX.Client.Mvvm.ViewModelFactory")]
@@ -9,19 +9,26 @@ using System.Reflection;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("LogoFX.Client.Mvvm.ViewModelFactory")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("40f35dc9-5c4e-4d6d-adfd-46740ec95f59")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.2.0")]
