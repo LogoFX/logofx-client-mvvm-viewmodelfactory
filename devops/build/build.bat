83a1586e1aa8c68e2202aea5ad74d37f30ee11bb
Gennady Verdel <godrose@gmail.com>
2021-03-23 11:06:21 +0200

    devops: added means for build and publish #7

---
+++ devops/build/build.bat
@@ -0,0 +1,3 @@
+cd ../../src
+dotnet build LogoFX.Client.Mvvm.ViewModelFactory.sln -c Release
+cd ../devops/publish
---
+++ devops/publish/publish.bat
@@ -0,0 +1,6 @@
+cd ../build
+call build.bat
+cd ../pack
+call ./pack.bat
+cd ../publish
+call ./copy.bat LogoFX.Client.Mvvm.ViewModelFactory. 2.2.0-rc2 %1
