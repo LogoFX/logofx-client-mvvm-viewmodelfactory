39306c9a4b2cb7009fb6e8fce3e082b602e90022
Gennady Verdel <godrose@gmail.com>
2021-03-23 11:10:38 +0200

    chore: minor

--- devops/publish/publish.bat
+++ devops/publish/publish.bat
@@ -3,4 +3,4 @@ call build.bat
 cd ../pack
 call ./pack.bat
 cd ../publish
-call ./copy.bat LogoFX.Client.Mvvm.ViewModelFactory. 2.2.0-rc2 %1
+call ./copy.bat LogoFX.Client.Mvvm.ViewModelFactory 2.2.0-rc2 %1
