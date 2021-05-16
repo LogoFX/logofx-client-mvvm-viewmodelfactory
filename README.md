75764651825543057ec33700cd019443a84e71d5
Gennady Verdel <godrose@gmail.com>
2021-05-16 11:38:43 +0300

    Merge CI/CD changes (#8)
    * chore: using source link
    
    * devops: adding means for automatic package creation #7

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 Definition and implementation of view model factory pattern
 
 <img src=https://ci.appveyor.com/api/projects/status/github/logofx/logofx-client-mvvm-viewmodelfactory>
+
+<img src=https://img.shields.io/nuget/dt/LogoFX.Client.Mvvm.ViewModelFactory>
+
+[Get package](https://www.nuget.org/packages/LogoFX.Client.Mvvm.ViewModelFactory)
---
+++ devops/publish/publish.bat
@@ -0,0 +1,13 @@
+SET package_name=LogoFX.Client.Mvvm.ViewModelFactory
+SET package_version=2.2.0-rc2
+cd ../../src
+nuget restore
+cd ../devops/build
+call build.bat
+cd ../pack
+call ./pack.bat
+cd ../publish
+call ./copy.bat %package_name% %package_version% %1
+cd ../install
+call uninstall-global-single.bat %package_name% %package_version%
+cd ..
